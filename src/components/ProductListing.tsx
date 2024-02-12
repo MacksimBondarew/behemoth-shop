23cf12067827ece12ae6bd76d90b4b8d0afc9044
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-02-12 21:30:25 +0100

    add: getProducts and started ui for their

---
+++ src/components/ProductListing.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Product } from "@/payload-types";
+import { useState } from "react";
+
+interface ProductListingProps {
+    product: Product | null;
+    index: number;
+}
+
+const ProductListing = ({ product, index }: ProductListingProps) => {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    if (!product || !isVisible) {
+        return <ProductPlaceholder />
+    }
+    const ProductPlaceholder = () => {
+        return (
+            <div className="flex flex-col w-full">
+                <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+ export default ProductListing;
