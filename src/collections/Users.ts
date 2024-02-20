e5172de4fa9276cbf11f8ff59eb0c2c0675d78fb
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-02-20 21:58:11 +0100

    add

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,19 +1,61 @@
-import { CollectionConfig } from "payload/types";
+import { PrimatyActionEmailHtml } from "@/components/emails/PrimaryActionEmail";
+import { Access, CollectionConfig } from "payload/types";
+
+const adminsAndUsers: Access = ({ req: { user } }) => {
+    if (user.role === "admin") {
+        return true;
+    }
+    return {
+        id: {
+            equals: user.id,
+        },
+    };
+};
 
 export const Users: CollectionConfig = {
     slug: "users",
     auth: {
         verify: {
             generateEmailHTML: ({ token }) => {
-                return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}"=>Verify here</a>`;
+                return PrimatyActionEmailHtml({
+                    actionLabel: "verify your email",
+                    buttonText: "Verify here",
+                    href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
+                });
             },
         },
     },
     access: {
-        read: () => true,
+        read: adminsAndUsers,
         create: () => true,
+        update: ({ req }) => req.user.role === "admin" && req.user,
+        delete: ({ req }) => req.user.role === "admin" && req.user,
+    },
+    admin: {
+        hidden: ({ user }) => user.role !== "admin",
+        defaultColumns: ["id"],
     },
     fields: [
+        {
+            name: "products",
+            label: "Products",
+            admin: {
+                condition: () => false,
+            },
+            type: "relationship",
+            relationTo: "products",
+            hasMany: true,
+        },
+        {
+            name: "product_files",
+            label: "Products files",
+            admin: {
+                condition: () => false,
+            },
+            type: "relationship",
+            relationTo: "product_files",
+            hasMany: true,
+        },
         {
             name: "role",
             required: true,
